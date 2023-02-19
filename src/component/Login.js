6ced045a70968c7b845bcae8b4169c3ba4ddd1d2
M Bilal <bilaldarmuhammad@gmail.com>
2023-02-19 16:57:59 +0500

    code update

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 function Login() {
     const login = () => {
-        localStorage.getItem('login', true)
+        localStorage.getItem('login', true);
+        navigate('/');
     }
+    const navigate = useNavigate();
+    useEffect(() => {
+        let login = localStorage.getItem('login');
+        if (login) {
+            navigate('/');
+        }
+    })
     return (
         <div>
             <h1>Login Page</h1>
@@ -12,7 +20,7 @@ function Login() {
             <br />
             <input type={'text'} />
             <br />
-            <button onClick={() => login}>Login</button>
+            <button onClick={login}>Login</button>
         </div>
     )
 }
--- src/component/Protected.js
+++ src/component/Protected.js
@@ -3,13 +3,13 @@ import { useNavigate } from 'react-router-dom';
 
 function Protected(props) {
     const { Component } = props;
-    const navigate = useNavigate
+    const navigate = useNavigate();
     useEffect(() => {
         let login = localStorage.getItem('login');
         if (!login) {
             navigate('/login')
         }
-    }, [])
+    })
 
     return (
         <div>
